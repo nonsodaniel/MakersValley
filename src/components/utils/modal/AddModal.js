93e66b5355b02f56df2b03b6478d89c1745952e5
nonsodaniel <nonsodaniel07@gmail.com>
2021-07-21 18:36:15 +0100

    Restyled modal header and added ID generator

--- src/components/utils/modal/AddModal.js
+++ src/components/utils/modal/AddModal.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
+import { uuid } from "./constants";
 import "./modal.scss";
 
 const customStyles = {
@@ -13,9 +14,17 @@ const customStyles = {
   },
 };
 
+if (typeof(window) !== 'undefined') {
+    Modal.setAppElement('body')
+}
 const AddModal = () => {
+   
+  const [todos, setTodos] = useState([])
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [title, setTitle] = useState("");
+  const [description, setDesc] = useState("");
+  const [prty, setPrty] = useState("");
+  const [category, setCategory] = useState("");
 
   const openModal = () => {
     setIsOpen(true);
@@ -29,9 +38,39 @@ const AddModal = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
-  console.log(title);
+  // const handleSave = () =>{
+    
+  // }
+  const addTodo = (e) => {
+    e.preventDefault()
+      let obj = {
+          id: uuid(),
+          title, description, 
+          priority: prty,
+          category,
+          status: "pending"
+      }
+      if(todos && todos.length){
+        let newData = [...todos, obj]
+        setTodos(newData)
+        localStorage.setItem("todos",  JSON.stringify(newData))
+        getTodo()
+      }else{
+        localStorage.setItem("todos", JSON.stringify([obj]))
+        getTodo()
+      }
+  };
+  const getTodo = () =>{
+    let data = JSON.parse(localStorage.getItem("todos"))
+    setTodos(data)
+  }
+  useEffect(() => {
+        getTodo()
+  }, [])
+
+  
   return (
-    <div className="modal">
+    <div className="todo-modal">
       <button className="btn" onClick={openModal}>
         Add Todo
       </button>
@@ -43,47 +82,51 @@ const AddModal = () => {
         contentLabel="Example Modal"
       >
         <div className="header">
-          <button className="text-center" onClick={closeModal}>
-            close
-          </button>
-        </div>
+        <span onClick={closeModal} className="text-right pointer"><i class="far fa-times-circle"></i></span>
         <h2 className="text-center">Creat a Todo</h2>
+        </div>
         <div className="body">
-          <form action="">
             <form>
-              <div class="form-group">
-                <label for="exampleFormControlInput1">Title</label>
+              <div className="form-group">
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="title"
                   placeholder="Todo Title"
                   value={title}
                   onChange={({ target }) => setTitle(target.value)}
                 />
               </div>
-              <div class="form-group">
-                <label for="exampleFormControlTextarea1">
-                  Todo Description
-                </label>
+              <div className="form-group">
                 <textarea
-                  class="form-control"
+                  className="form-control"
                   id="description"
                   rows="3"
+                  placeholder="Type your Todo Description here..."
+                  value={description}
+                  onChange={({ target }) => setDesc(target.value)}
                 ></textarea>
               </div>
-              <div class="form-group group-1">
+              <div className="form-group group-1">
                 <div className="priority">
-                  <label for="exampleFormControlSelect1">Priority</label>
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Less important</option>
-                    <option>Ver Important</option>
-                    <option>Not important</option>
+                  <select
+                    className="form-control"
+                    id="priority"
+                    onChange={({ target }) => setPrty(target.value)}
+                  >
+                    <option>Priority</option>
+                    <option value="less">Very Important</option>
+                    <option value="very">Very Important</option>
+                    <option value="not">Not important</option>
                   </select>
                 </div>
                 <div className="category">
-                  <label for="exampleFormControlSelect1">Category</label>
-                  <select class="form-control" id="exampleFormControlSelect1">
+                  <select
+                    className="form-control"
+                    id="exampleFormControlSelect1"
+                    onChange={({ target }) => setCategory(target.value)}
+                  >
+                    <option>Category</option>
                     <option>Work</option>
                     <option>Learning</option>
                     <option>Health</option>
@@ -95,10 +138,9 @@ const AddModal = () => {
               </div>
 
               <div className="btn-wrap">
-                <button className="btn">Submit Todo</button>
+                <button type="submit" className="btn" onClick={addTodo}>Submit Todo</button>
               </div>
             </form>
-          </form>
         </div>
       </Modal>
     </div>
